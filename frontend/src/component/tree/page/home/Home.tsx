022b59f8f8f462b97192f3d1545a83138e84b4a6
nabsba <ns-dev@outlook.com>
2021-12-28 14:43:11 +0100

    Style(variable.css): add space, line height & border

--- frontend/src/component/tree/page/home/Home.tsx
+++ frontend/src/component/tree/page/home/Home.tsx
@@ -19,6 +19,7 @@ const Home: React.FC = () => {
 	return (
 		<div>
 			<H1 title="TITLE 1" />
+			<Paragraph data="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris nisi tellus, molestie aliquam volutpat nec, dictum a eros. Praesent tempus ex sed augue luctus, et convallis eros suscipit. Nulla viverra enim in sem commodo vulputate. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Sed lobortis faucibus orci. Fusce ultricies metus in velit tempus aliquet euismod eget mauris. Aliquam ac mauris finibus, mollis nisi id, malesuada lacus. Nunc tristique elit dolor, at pulvinar felis vestibulum eget. Ut vel eros id velit dictum convallis. Aliquam sagittis tempus dui, ut porttitor sem aliquet id. Donec gravida, tortor ut efficitur maximus, mi massa pellentesque mauris, in sollicitudin augue turpis et quam. Phasellus interdum risus neque, a porta enim volutpat id. Fusce venenatis ipsum ligula, eu ultrices dui accumsan vitae. Mauris aliquet ornare urna. Sed ex purus, volutpat quis sapien at, porttitor scelerisque leo. Quisque scelerisque ex vitae arcu euismod, non malesuada lorem malesuada." />
 			<H2 title="TITLE 2" />
 			<H3 title="TITLE 3" />
 			<Paragraph data="Hello" />
