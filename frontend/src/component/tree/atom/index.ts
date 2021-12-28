56c109b4afb7086eab8292934caa22403cf3219b
nabsba <ns-dev@outlook.com>
2021-12-28 01:36:46 +0100

    Feat(atom/Span.tsx): add Span

--- frontend/src/component/tree/atom/index.ts
+++ frontend/src/component/tree/atom/index.ts
@@ -1,6 +1,7 @@
 import Paragraph from './paragraph/Paragraph';
+import Span from './span/Span';
 import H1 from './title/h1/H1';
 import H2 from './title/h2/H2';
 import H3 from './title/h3/H3';
 
-export { H1, H2, H3, Paragraph };
+export { H1, H2, H3, Paragraph, Span };
