3b766ce6f60bcbd769d07adb8a5e7670ea1bfff7
nabsba <ns-dev@outlook.com>
2021-12-29 18:58:39 +0100

    Feat(atom/index.ts): add ImageAsComponent

--- frontend/src/component/tree/atom/index.ts
+++ frontend/src/component/tree/atom/index.ts
@@ -1,3 +1,4 @@
+import ImageAsComponent from './image/Image';
 import Paragraph from './paragraph/Paragraph';
 import Span from './span/Span';
 import SubText from './subText/SubText';
@@ -5,4 +6,4 @@ import H1 from './title/h1/H1';
 import H2 from './title/h2/H2';
 import H3 from './title/h3/H3';
 
-export { H1, H2, H3, Paragraph, Span, SubText };
+export { H1, H2, H3, Paragraph, Span, SubText, ImageAsComponent };
