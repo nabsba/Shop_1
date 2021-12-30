e48e15140b6557b80dba74be5730ba4ac783c473
nabsba <ns-dev@outlook.com>
2021-12-30 09:17:24 +0100

    Feat(service/Common: add function.ts with onPressEntry

---
+++ frontend/src/service/Common/funtions.ts
@@ -0,0 +1,13 @@
+const toggleBoolean = (entry: boolean) => !entry;
+const onPressEntry = (
+	event: React.KeyboardEvent,
+	// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+	myFunction: any,
+): void => {
+	const enterOrSpace = event.key === 'Enter' || event.key === 'Spacebar';
+	if (enterOrSpace) {
+		myFunction;
+	}
+};
+
+export { toggleBoolean, onPressEntry };
