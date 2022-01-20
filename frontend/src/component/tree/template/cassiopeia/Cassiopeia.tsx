1ef8277b9214f1c2b65fdf981e49df597f3ad0e1
R.S <r.s@RSs-MacBook-Pro.local>
2022-01-20 16:18:06 +0000

    Feat(Cassiopeia.tsx): add errror fallback

--- frontend/src/component/tree/template/cassiopeia/Cassiopeia.tsx
+++ frontend/src/component/tree/template/cassiopeia/Cassiopeia.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import ErrorBoundaryFallback from '../../../specialCase/errorBundary/ErrorBundaryFallback';
 import { CircularIndeterminate, H3 } from '../../atom';
 import {
 	ArticleGroupOriginal,
@@ -9,7 +10,8 @@ import {
 } from '../../organism';
 import './style.css';
 import TCassiopeia from './type';
-
+import { ErrorBoundary } from 'react-error-boundary';
+import { ERROR_CODE } from '../../../../Common/constant';
 type Props = {
 	data: TCassiopeia;
 };
@@ -39,25 +41,34 @@ const Cassiopeia: React.FC<Props> = ({
 			<section className="cassiopeia_section_2">
 				<HeaderProduct data={headerProductWithFunction} />
 			</section>
-			<section
-				className={`cassiopeia_section_3 ${
-					displayFilterProduct ? '' : 'cassiopeia_section_3_effect'
-				}`}
+			<ErrorBoundary
+				fallbackRender={() => (
+					<ErrorBoundaryFallback
+						type={'products'}
+						code={ERROR_CODE.FETCH_PRODUCTS}
+					/>
+				)}
 			>
-				{!articleGroupOriginal.pending.products && (
-					<FilterProduct data={{ filteringCategories }} />
-				)}
-				{articleGroupOriginal.pending.productsBeingFiltered ||
-				articleGroupOriginal.pending.products ? (
-					<div className="cassiopeia_loader">
-						<CircularIndeterminate />
-					</div>
-				) : articleGroupOriginal.display ? (
-					<ArticleGroupOriginal data={articleGroupOriginal} />
-				) : (
-					<H3 title={'No products'} />
-				)}
-			</section>
+				<section
+					className={`cassiopeia_section_3 ${
+						displayFilterProduct ? '' : 'cassiopeia_section_3_effect'
+					}`}
+				>
+					{!articleGroupOriginal.pending.products && (
+						<FilterProduct data={{ filteringCategories }} />
+					)}
+					{articleGroupOriginal.pending.productsBeingFiltered ||
+					articleGroupOriginal.pending.products ? (
+						<div className="cassiopeia_loader">
+							<CircularIndeterminate />
+						</div>
+					) : articleGroupOriginal.display ? (
+						<ArticleGroupOriginal data={articleGroupOriginal} />
+					) : (
+						<H3 title={'No products'} />
+					)}
+				</section>
+			</ErrorBoundary>
 			<section className="cassiopeia_section_4">
 				<Footer data={footer} />
 			</section>
