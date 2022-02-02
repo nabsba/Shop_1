a36419f541551f8e2e541383eb3920b2839ef965
nabsba <ns-dev@outlook.com>
2022-02-02 23:46:46 +0000

    Refactor(ErrorBundaryFallBack.tsx): replace tags with atom's component

--- frontend/src/component/specialCase/errorBundary/ErrorBundaryFallback.tsx
+++ frontend/src/component/specialCase/errorBundary/ErrorBundaryFallback.tsx
@@ -1,33 +1,34 @@
 import React from 'react';
-import { ADMINISTRATION } from '../../../Common/constant';
-import { Span } from '../../tree/atom';
+import { ADMINISTRATION } from '../../../service/Common/constant';
+import { H3, Paragraph, Span } from '../../tree/atom';
 import NavLink from '../../tree/atom/link/NavLink';
 import './style.css';
 import TErrorBundaryFallBack from './type';
 
 const ErrorBoundaryFallback: React.FC<TErrorBundaryFallBack> = ({
 	type,
+	code,
 }: {
 	type: string;
 	code: number;
 }) => {
 	return (
 		<div id="error-bundary">
-			<h1>
-				A {type} should have been displayed but we are we are encountering some
-				issues...
-			</h1>
-			<p>
-				In case if the issue is not corrected within 24 hours, please send us a
-				message to
-				<span>
-					<NavLink data={{ text: 'email', href: ADMINISTRATION.EMAIL }} />
-				</span>
-				including the code &apos;{type}&apos; as reference.
-			</p>
+			<H3
+				title={`${type} should have been displayed but we are encountering some
+				issues...`}
+			/>
+			<Paragraph
+				data={
+					'In case if the issue is not corrected within 24 hours, please send us a message to'
+				}
+			/>
+			<NavLink data={{ text: 'email', href: ADMINISTRATION.EMAIL }} />
+			<Paragraph data={`including the code: "${code}" as reference.`} />
 			<Span data={'Thank you'} />
 		</div>
 	);
 };
 
 export default ErrorBoundaryFallback;
+//				including the code &apos;{type}&apos; as reference.
